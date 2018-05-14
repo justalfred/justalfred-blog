06c980b5773d1932158d38d80aec6b87d38adabd
Alfred Lee <alfred.k.lee@gmail.com>
2018-05-14 18:07:41 -0400

    update settings

--- publishconf.py
+++ publishconf.py
@@ -22,4 +22,4 @@ DELETE_OUTPUT_DIRECTORY = False
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = "UA-58159452-1"
+#GOOGLE_ANALYTICS = "UA-58159452-1"
